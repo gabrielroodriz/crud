775e904c7bb75f85ac40850b64c1194a5fbe2a58
Gabriel <gabrielroodriz@gmail.com>
2020-03-05 18:02:55 -0300

    Fixing(frontend): Criação do component home

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../templates/Main";
+
+export default props => (
+  <Main icon="home" title="Início" subtitle="Segundo Projeto Banana Fish">
+    <div className="display-4">Bem Vindo!</div>
+    <hr />
+    <p className="mb-0">Sistema de cadastro em React</p>
+  </Main>
+);
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -5,14 +5,14 @@ import React from "react";
 
 import Logo from "../components/templates/Logo";
 import Nav from "../components/templates/Nav";
-import Main from "../components/templates/Main";
+import Home from "../components/home/Home";
 import Footer from "../components/templates/Footer";
 
 export default props => (
   <div className="app">
     <Logo />
     <Nav />
-    <Main icon="home" title="Início" subtitle="Segundo Projeto Banana Fish" />
+    <Home />
     <Footer />
   </div>
 );
