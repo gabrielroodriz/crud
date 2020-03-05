9fb2148703a0c69285631d9960fec1f97f8ef149
Gabriel <gabrielroodriz@gmail.com>
2020-03-05 17:43:40 -0300

    Fixing(Frontend): Estilizando o Footer

--- frontend/src/components/templates/Footer.jsx
+++ frontend/src/components/templates/Footer.jsx
@@ -1,4 +1,10 @@
 import "./Footer.css";
 import React from "react";
 
-export default props => <footer className="footer">Rodapé</footer>;
+export default props => (
+  <footer className="footer">
+    <span>
+      Desenvolvido com <i className="fa fa-heart text-danger"></i> <strong>Rocour</strong>
+    </span>
+  </footer>
+);
